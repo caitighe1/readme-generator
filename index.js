226ca480a988e78c397e1a933e973299f4f755f8
Caitlin Tighe <caitighe@gmail.com>
2021-04-01 15:13:53 -0400

    added index.js file and added inquirer prompt

---
+++ index.js
@@ -0,0 +1,16 @@
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
