90750beca195319b38d8b28eac6796cdd20835f5
Caitlin Tighe <caitighe@gmail.com>
2021-04-01 15:59:23 -0400

    debugged node, was not picking up index.js file, added more questions

--- index.js
+++ index.js
@@ -6,17 +6,31 @@ inquirer
         type: 'input',
         name: 'input_type',
         message: 'What is your name?',
-        default: 'Caitlin'
-    }
+        default: 'Caitlin',
+    },
+    {
+        type: 'list',
+        name: 'list question',
+        message: 'What programming language do you like?',
+        choices:['Javascript', 'C++', 'Java', 'Python'],
+        default: 'Javascript'
+    },
+    {
+        type: 'checkbox',
+        name: 'checkbox question',
+        message: 'What borough do you live in?',
+        choices:['Queens', 'Brooklyn', 'Bronx', 'Manhattan'],
+        default: 'Queens'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github name?',
+        default: 'caitighe1'
+    },
   ])
   .then(answers => {
     // Use user feedback for... whatever!!
-    console.log(answers)
+    console.log(answers);
   })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+  
