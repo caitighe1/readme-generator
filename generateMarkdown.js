3433f6ca2e324c2589860e8c6fc19d2fefa2ed68
Caitlin Tighe <caitighe@gmail.com>
2021-04-03 21:25:53 -0400

    created function to render license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]`;
+      break;
+    case "No License":
+      licenseBadge = ``;
+      break;
+  }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
